2c8e49d221ceb32e0d0572d2a61e4f121a1604f8
bcamishrapr <prassonmishra330@gmail.com>
2021-07-15 05:18:34 +0000

    Add new file

---
+++ vars/mail-send.groovy
@@ -0,0 +1,11 @@
+def mail (Map stage) {
+       
+	   emailext (
+	        to:  stage.to
+			body: stage.body,
+			mimeType: 'text/html',
+			subject: stage.sub,
+			replyTo: '$DEFAULT_REPLYTO',
+			attachmentsPattern: 'test.txt'
+	   )
+}
